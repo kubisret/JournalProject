d19ab3838e86d18763a57a83f9d8967f611dbbf3
aleksejzavalov <avatarcum@mail.ru>
2024-03-30 02:15:12 +0500

    Добавлены модели регистрации и авторизации

--- data/users.py
+++ data/users.py
@@ -1,5 +1,6 @@
 import datetime
 import sqlalchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from .db_session import SqlAlchemyBase
 
 
@@ -8,7 +9,13 @@ class User(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    role = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, url_for, render_template, redirect
+from flask import Flask, url_for, render_template, redirect, request
 from forms.login_form import LoginForm
+from forms.user import RegisterForm
 from data import db_session
 from data.users import User
 
@@ -9,21 +10,50 @@ app.config['SECRET_KEY'] = 'journalproject_secret_key'
 
 def main():
     db_session.global_init("db/blogs.db")
-    app.run()
+    app.run(debug=True)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Журнал')
+    return render_template('index.html', title='Электронный журнал')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    if request.method == 'POST':
+        username = request.form['email']
+        password = request.form['password']
+        remember_me = request.form['remember_me']
+
+        print(f'Username: {username}, Password: {password}, {remember_me}')
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
     if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('login.html', title='Авторизация', form=form)
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
