2944453da65602e6cacf164a0f2ed88b3acc66ae
aleksejzavalov <avatarcum@mail.ru>
2024-03-30 21:30:20 +0500

    Настроена регистрация и авторизация пользователей

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask import Flask, url_for, render_template, redirect, request
 from forms.login_form import LoginForm
 from forms.user import RegisterForm
@@ -21,14 +22,17 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['email']
-        password = request.form['password']
-        remember_me = True if request.form.get('remember_me', False) else False
-
-        print(f'Username: {username}, Password: {password}, {remember_me}')
-
-    return render_template('login.html', title='Регистрация')
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
