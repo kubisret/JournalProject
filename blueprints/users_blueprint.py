5a843cdba15f7b7a07e99c54f763458f1d4de739
Nitor112221 <kostya1234moi@gmail.com>
2024-03-31 18:51:11 +0500

    все обработчики связанные с регистрацией и авторизацией вынесены в отдельный файл в виде blueprint'ов

---
+++ blueprints/users_blueprint.py
@@ -0,0 +1,118 @@
+import flask
+from flask import redirect, render_template, flash
+from flask_login import login_manager, login_required, logout_user, current_user, login_user
+
+from data import db_session
+from data.reset_password_email import send_reset_password_email
+from data.users import User
+from forms.login_form import LoginForm
+from forms.reset_forms import ResetPasswordRequestForm, ResetPasswordForm
+from forms.user import RegisterForm
+
+blueprint = flask.Blueprint(
+    'users_funciton',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect("/index")
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильная почта или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@blueprint.route('/register', methods=['GET', 'POST'])
+def reqister():
+    if current_user.is_authenticated:
+        return redirect("/index")
+
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@blueprint.route('/reset_password', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect("/index")
+
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if not user:
+            return redirect('/register')
+        else:
+            send_reset_password_email(user, blueprint)
+
+            flash(
+                "Instructions to reset your password were sent to your email address,"
+                " if it exists in our system."
+            )
+
+            render_template('reset_password_request.html', form=form)
+
+    return render_template('reset_password_request.html', form=form)
+
+
+@blueprint.route("/reset_password/<token>/<int:user_id>", methods=["GET", "POST"])
+def reset_password(token, user_id):
+    if current_user.is_authenticated:
+        return redirect("/index")
+    db_sess = db_session.create_session()
+    user = User.validate_reset_password_token(token, user_id, db_sess, blueprint)
+    if not user:
+        return render_template(
+            "reset_password_error.html", title="Reset Password error"
+        )
+
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db_sess.commit()
+
+        return render_template(
+            "reset_password_success.html", title="Reset Password success"
+        )
+
+    return render_template(
+        "reset_password.html", title="Reset Password", form=form
+    )
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import os
 
-from flask import Flask, url_for, render_template, redirect, request, session, flash
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask import Flask, render_template
+from flask_login import LoginManager
 from flask_mailman import Mail
 
-from data.reset_password_email import send_reset_password_email
-from forms.login_form import LoginForm
-from forms.reset_forms import ResetPasswordRequestForm, ResetPasswordForm
-from forms.user import RegisterForm
+from blueprints import users_blueprint
 from data import db_session
 from data.users import User
 
@@ -30,14 +27,8 @@ def main():
     if not os.path.exists('db'):
         os.makedirs('db')
     db_session.global_init("db/journal.db")
-
+    app.register_blueprint(users_blueprint.blueprint)
     app.run()
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title='Электронный журнал')
 
 
 @login_manager.user_loader
@@ -46,106 +37,10 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect("/")
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect("/index")
-
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильная почта или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    if current_user.is_authenticated:
-        return redirect("/index")
-
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        user = User(
-            name=form.name.data,
-            surname=form.surname.data,
-            email=form.email.data,
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
-
-
-@app.route('/reset_password', methods=['GET', 'POST'])
-def reset_password_request():
-    if current_user.is_authenticated:
-        return redirect("/index")
-
-    form = ResetPasswordRequestForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if not user:
-            return redirect('/register')
-        else:
-            send_reset_password_email(user, app)
-
-            flash(
-                "Instructions to reset your password were sent to your email address,"
-                " if it exists in our system."
-            )
-
-            render_template('reset_password_request.html', form=form)
-
-    return render_template('reset_password_request.html', form=form)
-
-
-@app.route("/reset_password/<token>/<int:user_id>", methods=["GET", "POST"])
-def reset_password(token, user_id):
-    if current_user.is_authenticated:
-        return redirect("/index")
-    db_sess = db_session.create_session()
-    user = User.validate_reset_password_token(token, user_id, db_sess, app)
-    if not user:
-        return render_template(
-            "reset_password_error.html", title="Reset Password error"
-        )
-
-    form = ResetPasswordForm()
-    if form.validate_on_submit():
-        user.set_password(form.password.data)
-        db_sess.commit()
-
-        return render_template(
-            "reset_password_success.html", title="Reset Password success"
-        )
-
-    return render_template(
-        "reset_password.html", title="Reset Password", form=form
-    )
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Электронный журнал')
 
 
 if __name__ == '__main__':
