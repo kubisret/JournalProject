ceb7dc423cebfe9e5e888f004ceb7d9bf9905366
aleksejzavalov <avatarcum@mail.ru>
2024-04-03 19:28:22 +0500

    Изменены названия файлов

--- static/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $("#label-password-register").keyup(function () {
-        var pass = $("#label-password-register").val();
-        $("#result").text(pass);
-    });
-});
