875fd4940c6dd5fefba7bd79e41ec17c3de46ec4
Nitor112221 <kostya1234moi@gmail.com>
2024-04-28 11:05:29 +0500

    обновлена модель для дз

--- data/models/homework.py
+++ data/models/homework.py
@@ -9,10 +9,11 @@ class Homework(SqlAlchemyBase):
     __tablename__ = 'homework'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    about = sqlalchemy.Column(sqlalchemy.TEXT, nullable=True)
+    text = sqlalchemy.Column(sqlalchemy.TEXT, nullable=True)
+    recipient = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
-    id_class = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('classes.id'))
-    is_end = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
+    date = sqlalchemy.Column(sqlalchemy.DateTime)
 
+    id_class = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('classes.id'))
+    file_name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     classes = orm.relationship('Classes')
